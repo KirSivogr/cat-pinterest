dc2263c5a7987b0ffbbf896f62a6aa7405655f94
sivog <sivograkovkirill@mail.ru>
2025-01-18 23:52:25 +0300

    add favorite page

---
+++ src/components/pages/FavoritesPage.tsx
@@ -0,0 +1,12 @@
+import { FavoriteCats } from '@components/favorite-cats/FavoriteCats';
+
+import { Header } from '@/components/header/Header';
+
+export const FavoritesPage = () => {
+   return (
+      <>
+         <Header />
+         <FavoriteCats />
+      </>
+   );
+};
