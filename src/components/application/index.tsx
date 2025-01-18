d7603eb2f13b271b8fe0edd2396bef1cf62c73ee
sivog <sivograkovkirill@mail.ru>
2025-01-18 23:53:38 +0300

    add app component

---
+++ src/components/application/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { FavoritesPage } from '@components/pages/FavoritesPage';
+import { HomePage } from '@components/pages/HomePage';
+
+export const App = () => {
+   return (
+      <BrowserRouter>
+         <main>
+            <Routes>
+               <Route path='/' element={<HomePage />} />
+               <Route path='/favorites' element={<FavoritesPage />} />
+            </Routes>
+         </main>
+      </BrowserRouter>
+   );
+};
